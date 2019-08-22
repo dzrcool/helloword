9c431b8efdc37607a5250c8ece3cda376206fc95
dzrcool <dongzongran@qq.com>
2019-08-22 14:13:10 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 print('hello world!')
 
 print('dzr')
+<<<<<<< HEAD
 
 a = 10
 b = 20
@@ -8,3 +9,5 @@ print(a+b)
 
 for i in range(10):
     print(i)
+=======
+>>>>>>> parent of ed7a1aa... Update test.py
